14768ce012a494b4ab5c756173b90a22530387f5
Alexander Kerner <alexkerner1000@gmail.com>
2022-04-23 18:40:54 +0200

    Add examples

---
+++ terra-account-stats-app/src/test/java/com/github/kerner1000/terra/data/ExecuteMessageTest.java
@@ -0,0 +1,35 @@
+package com.github.kerner1000.terra.data;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ExecuteMessageTest {
+
+    @Test
+    void testTerraswap() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        ExecuteMessage messages = objectMapper.readValue(new File("src/test/resources/example-execute-message-terraswap.json"), new TypeReference<>() {
+        });
+        assertNotNull(messages);
+        assertNotNull(messages.getSwap());
+    }
+
+    @Test
+    void testMarket() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        ExecuteMessage messages = objectMapper.readValue(new File("src/test/resources/example-execute-message-market.json"), new TypeReference<>() {
+        });
+        assertNotNull(messages);
+        AssertLimitOrder assertLimitOrder = messages.getAssertLimitOrder();
+        assertNotNull(assertLimitOrder);
+    }
+}
